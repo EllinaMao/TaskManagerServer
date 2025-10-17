10718e7049d1967ab45b48b2cc051b569a28825f
Olga <xotaoshi@gmail.com>
2025-10-18 00:01:59 +0300

    Add project files.

---
+++ TaskManagerServer/Control.cs
@@ -0,0 +1,118 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TaskManagerServer
+{
+    public class ControlAsync
+    {
+        private int _port;
+        private Socket _listener;
+        private bool _running;
+
+
+        public event Action<string> LogMessage;
+        public event Action<string> ClientConnected;
+        public event Action<string> ClientDisconnected;
+
+        private SynchronizationContext _uiContext = null;//winforms
+
+        public ControlAsync(int port = 49200, SynchronizationContext uiContext = null)
+        {
+            _port = port;
+            _uiContext = uiContext ?? new SynchronizationContext();
+
+        }
+        private void Log(string msg)
+        {
+            if (_uiContext != null)
+                _uiContext.Post(d => LogMessage?.Invoke(msg), null);
+            else
+                LogMessage?.Invoke(msg);
+        }
+
+        public async Task StartAsync()
+        {
+            _listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any, 49200);
+            _listener.Bind(ipEndPoint);
+            _listener.Listen(100);
+            _running = true;
+
+            Log($"Server started on port {_port}");
+
+            while (_running)
+            {
+                try
+                {
+                    var clientSocket = await _listener.AcceptAsync();
+                    string? ep = clientSocket?.RemoteEndPoint?.ToString();
+                    ClientConnected?.Invoke(ep);
+                    _ = HandleClientAsync(clientSocket, ep);
+                }
+                catch (Exception ex)
+                {
+                    Log("Accept error: " + ex.Message);
+                }
+            }
+        }
+        public void Stop()
+        {
+            _running = false;
+            try
+            {
+                _listener.Close();
+            }
+            catch (Exception ex)
+            {
+                Log("Close error: " + ex.Message);
+            }
+        }
+        private async Task HandleClientAsync(Socket client, string ep)
+        {
+            try
+            {
+                byte[] bytes = new byte[1024];
+                int total = 0;
+                while (_running)
+                {
+                    int bytesRec = await client.ReceiveAsync(bytes, SocketFlags.None);
+                    if (bytesRec == 0)
+                    {
+                        //close socket
+                        break;
+                    }
+                    total += bytesRec;
+                }
+                string json = Encoding.UTF8.GetString(bytes, 0, total);
+                //await HandleCommandAsync(client, json, ep);
+            }
+            catch (Exception ex)
+            {
+                Log($"Client {ep} error {ex.Message}");
+            }
+            finally
+            {
+                try
+                {
+                    Stop();
+                }
+                catch (Exception ex)
+                {
+                    {
+                        ClientDisconnected?.Invoke(ep);
+                    }
+
+                }
+
+            }
+        }
+
+        //private async Task HandleCommandAsync(Socket client, string json, string ep)
+        //{
+
+        //}
+    }
+}
---
+++ TaskManagerServer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManagerServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
